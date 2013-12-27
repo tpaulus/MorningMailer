91b03f7354dc8918ff8888559ac23eb88417e30e
Tom Paulus <tom@tompaulus.com>
2013-12-26 18:33:45 -0800

    Improved Email Sending Module, now more stable and supports a MIME Multipart message.

--- modules/SendEmail.py
+++ modules/SendEmail.py
@@ -3,6 +3,7 @@
 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from email.utils import formatdate
 from properties import Property
 
@@ -15,18 +16,30 @@ class mail(object):
         self.smtp_port = Property.email_port
         self.smtpserver = smtplib.SMTP(self.smtp_server, self.smtp_port)
 
-    def send(self, to, subject, message):
+    def send(self, to, subject, message_html):
+
         try:
+            msgRoot = MIMEMultipart('related')
+            msgRoot['Subject'] = subject
+            msgRoot['From'] = Property.email_from
+            msgRoot['To'] = to
+            msgRoot['Date'] = formatdate(localtime=True)
+
+            msgAlternative = MIMEMultipart('alternative')
+            msgRoot.attach(msgAlternative)
+
+            # TODO add plaintext attachment to the email
+            # msgText = MIMEText(message_plain, _charset='utf-8')
+            # msgAlternative.attach(msgText)
+
+            msgText = MIMEText(message_html, 'html', _charset='utf-8')
+            msgAlternative.attach(msgText)
+
             self.smtpserver.ehlo()
             self.smtpserver.starttls()
             self.smtpserver.ehlo()
             self.smtpserver.login(self.smtp_user, self.smtp_password)
-            msg = MIMEText(message, 'html', _charset='utf-8')
-            msg['Subject'] = subject
-            msg['From'] = Property.email_from
-            msg['To'] = to
-            msg['Date'] = formatdate(localtime=True)
-            self.smtpserver.sendmail(self.smtp_user, [to], msg.as_string())
+            self.smtpserver.sendmail(self.smtp_user, [to], msgRoot.as_string())
             self.smtpserver.quit()
             return True
 
@@ -37,7 +50,8 @@ class mail(object):
 if __name__ == '__main__':
     Email = mail()
     test_email = raw_input("What is your email? ")
-    if Email.send(test_email, 'TEST', 'Hello World,\n\tThis is a test!'):
+    message = 'Hello World,\n\tThis is a test!'
+    if Email.send(test_email, 'TEST', message):  # HTML and Plaintext are the same in this case
         pass
     else:
         quit("Mail Send Error")
